e2e9515e1bc21f231b0e85151247b9d1984c14d1
jlelis <jerfao@hotmail.com>
2021-08-11 12:14:05 -0300

    capitulo 1

---
+++ client/app/index.js
@@ -0,0 +1,30 @@
+const date = document.getElementById('data');
+date.value = new Date().toISOString().split('T')[0].slice(0, 10);
+var campos = [
+    document.querySelector('#data'),
+    document.querySelector('#valor'),
+    document.querySelector('#quantidade')
+];
+console.log(campos); // verificando o conteúdo do array
+var tbody = document.querySelector('table tbody');
+document.querySelector('.form').addEventListener('submit', function (event) {
+    event.preventDefault();
+    var tr = document.createElement('tr');
+    campos.forEach(function (campo) {
+        var td = document.createElement('td');
+        td.textContent = campo.value;
+        tr.appendChild(td);
+    });
+    var tdVolume = document.createElement('td');
+    tdVolume.textContent = campos[1].value * campos[2].value;
+    tr.appendChild(tdVolume);
+    tbody.appendChild(tr);
+    // limpa o campo da data
+    //campos[0].value = '';
+    // limpa o campo da quantidade
+    campos[1].value = 1;
+    // limpa o campo do valor
+    campos[2].value = 1;
+    // foca no campo da data
+    campos[0].focus();
+});
