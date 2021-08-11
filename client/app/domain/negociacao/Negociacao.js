5410f84eb276f0026d760b3866aee150312ab568
jlelis <jerfao@hotmail.com>
2021-08-11 19:51:20 -0300

    cap. 2

---
+++ client/app/domain/negociacao/Negociacao.js
@@ -0,0 +1,8 @@
+// client/app/domain/negociacao/Negociacao.js
+class Negociacao {
+    constructor(data, quantidade, valor) {
+        this.data = new Date(); // data atual
+        this.quantidade = 1;
+        this.valor = 0.0;
+    }
+}
